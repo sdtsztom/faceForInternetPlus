f4b9baf6c6ef772e98bd01fd24b2e82ac6e4d982
sdtsztom <sdtsztom@163.com>
2020-05-28 19:58:31 +0800

    修复计算上的bug和播放完视频后报错的bug

--- MyWindow.py
+++ MyWindow.py
@@ -20,19 +20,16 @@ class m_MainWindow(QMainWindow):
 		self.ui.labelVideo0.setMouseTracking(True)
 		self.flagFirstFrame = False
 		self.labelVideoRec = self.ui.labelVideo0.geometry().getRect()
-		self.tracker=Tracker()
+		self.tracker=Tracker('/home/visitor16/czy/pysot/experiments/siamrpn_r50_l234_dwxcorr/config.yaml','/home/visitor16/czy/pysot/experiments/siamrpn_r50_l234_dwxcorr/model.pth')
+		self.slotInit()
 
 	def slotInit(self):
 		self.timerVideo0.timeout.connect(self.showVideo0)
 
 	def showVideoButtonClicked(self):
 		if self.timerVideo0.isActive() == False:
-			flag = self.cap0.open(self.video0Addr)
-			if flag == False:
-				QtWidgets.QMessageBox.warning(self, "Warning", "请检测相机与电脑是否连接正确")
-			else:
-				self.timerVideo0.start(30)  # ms,about 30fps
-				self.ui.buttonStart.setText('停止')
+			self.timerVideo0.start(30)  # ms,about 30fps
+			self.ui.buttonStart.setText('停止')
 		else:
 			self.timerVideo0.stop()
 			self.cap0.release()
@@ -44,14 +41,22 @@ class m_MainWindow(QMainWindow):
 		self.video0Addr, _ = QFileDialog.getOpenFileName(self, '选择文件')
 		self.flagFirstFrame = True  # 只有选择完文件后才有第一帧
 		self.ui.labelFilePath.setText(self.video0Addr)
-		ret,self.firstFrame=self.cap0.read()
-		self.video0Resolution=[self.firstFrame.shape[1],self.firstFrame.shape[0]] # [w,h]
-		self.showCV2CapRawImage(self.ui.labelVideo0,self.firstFrame)
+		flag = self.cap0.open(self.video0Addr)
+		if flag == False:	# 打开失败
+			QtWidgets.QMessageBox.warning(self, "Warning", "请检测相机与电脑是否连接正确")
+		else:
+			ret,self.firstFrame=self.cap0.read()
+			self.video0Resolution=[self.firstFrame.shape[1],self.firstFrame.shape[0]] # [w,h]
+			self.showCV2CapRawImage(self.ui.labelVideo0,self.firstFrame)
 
 	def showVideo0(self):
 		flag, frame = self.cap0.read()
-		frame=self.tracker.track(frame)
-		self.showCV2CapRawImage(self.ui.labelVideo0, frame)
+		if flag:
+			frame=self.tracker.track(frame)
+			self.showCV2CapRawImage(self.ui.labelVideo0, frame)
+		else:	# read over
+			self.timerVideo0.stop()
+			self.ui.buttonStart.setText('开始')
 
 	def showCV2CapRawImage(self, label, rawImage):
 		_, _, w, h = label.geometry().getRect()  # geometry() return QRect
@@ -72,7 +77,6 @@ class m_MainWindow(QMainWindow):
 			box = self.calBox()
 			self.flagFirstFrame = False
 			self.tracker.track(self.firstFrame,box)
-			self.slotInit()
 
 	def calBox(self):
 		WlabelVideo=self.labelVideoRec[2]
@@ -81,8 +85,8 @@ class m_MainWindow(QMainWindow):
 		endPos2LabelVideo = [self.recEnd[0] - self.labelVideoRec[0], self.recEnd[1] - self.labelVideoRec[1]]
 		ratioXStart = startPos2LabelVideo[0] / WlabelVideo
 		ratioYStart = startPos2LabelVideo[1] / HlabelVideo
-		ratioW = endPos2LabelVideo[0]-startPos2LabelVideo[0] / WlabelVideo
-		ratioH = endPos2LabelVideo[1]-startPos2LabelVideo[1] / HlabelVideo
+		ratioW = (endPos2LabelVideo[0]-startPos2LabelVideo[0]) / WlabelVideo
+		ratioH = (endPos2LabelVideo[1]-startPos2LabelVideo[1]) / HlabelVideo
 		frameW,frameH=self.video0Resolution
 		return [frameW*ratioXStart, frameH*ratioYStart, frameW*ratioW, frameH*ratioH]
 
