fdffaa53f1864c9148538b458b7929e68d0013e9
sdtsztom <sdtsztom@163.com>
2020-05-29 22:20:45 +0800

    适配吴老师要求： 1. 多线程调用模型跑结果 2. 字体变大 3. 视频循环播放 4. 上一版传错了MyWindows.py文件

--- MyWindow.py
+++ MyWindow.py
@@ -5,6 +5,8 @@ from myUI import Ui_MainWindow
 from PyQt5 import QtCore,QtWidgets,QtGui
 from PyQt5.QtWidgets import QApplication,QMainWindow,QFileDialog
 import cv2
+import time,threading
+from run_demo import *
 
 
 class m_MainWindow(QMainWindow):
@@ -30,20 +32,31 @@ class m_MainWindow(QMainWindow):
 			else:
 				self.timerVideo0.start(30) # ms,about 30fps
 				self.ui.buttonStart.setText('停止')
-		else:
+				self.ui.labelOutput.setText('正在等待运行结果...')
+				self.runModelThread=threading.Thread(target=self.runModel,name='runModelThread')
+				self.runModelThread.start()
+				self.threadRunning=True
+		else:	# 停止按钮按下
 			self.timerVideo0.stop()
 			self.cap0.release()
 			self.ui.labelVideo0.clear()
 			self.ui.labelFilePath.setText('尚未选择文件')
 			self.ui.buttonStart.setText('开始')
+			self.ui.labelOutput.setText('')
+			#if self.threadRunning:
+				#self.runModelThread.terminate()
+			delete_frames()
 
 	def chooseFileButtonClicked(self):
 		self.video0Addr,_=QFileDialog.getOpenFileName(self,'选择文件')
 		self.ui.labelFilePath.setText(self.video0Addr)
 		
 	def showVideo0(self):
-		flag, image = self.cap0.read()
-		self.showCV2CapRawImage(self.ui.labelVideo0,image)
+		flag, frame = self.cap0.read()
+		if flag:
+			self.showCV2CapRawImage(self.ui.labelVideo0, frame)
+		else:	# read over
+			self.cap0.open(self.video0Addr)
 
 
 	def showCV2CapRawImage(self,label,rawImage):
@@ -52,6 +65,14 @@ class m_MainWindow(QMainWindow):
 		img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 		Qimg = QtGui.QImage(img.data, img.shape[1], img.shape[0], QtGui.QImage.Format_RGB888)
 		label.setPixmap(QtGui.QPixmap.fromImage(Qimg))
+
+	def runModel(self):
+		delete_frames()
+		modelOutput=run(self.video0Addr)
+		print('model run over!')
+		self.ui.labelOutput.setText(modelOutput)
+		self.threadRunning=False
+		delete_frames()
 
 app=QApplication(sys.argv)
 main_window = m_MainWindow()
